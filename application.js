2983a9963a3a83b159dd88c92565fb1e1eb8be3f
AreYouCerf <limansk123@yandex.ru>
2026-09-06 23:17:36 +0300

    The second task

--- application.js
+++ application.js
@@ -1,2 +1,15 @@
 const greeting = document.querySelector('.greeting');
 greeting.textContent = 'Доброго времени суток!'
+
+let counter = 0
+const outer = document.querySelector('.counter');
+document.querySelector('.buttonPlus').addEventListener('click', () => {
+  counter = counter + 100000;
+  outer.textContent = counter;
+});
+document.querySelector('.buttonMinus').addEventListener('click', () => {
+  if (counter >= 100000) {
+    counter = counter - 100000;
+    outer.textContent = counter;
+  }
+})
