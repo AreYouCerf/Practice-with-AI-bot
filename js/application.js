7f9888826220de83ed66cb0ee4ae1bede6c8f52b
AreYouCerf <limansk123@yandex.ru>
2026-09-16 00:36:15 +0300

    structured

--- js/application.js
+++ js/application.js
@@ -397,25 +397,3 @@ document.addEventListener('DOMContentLoaded', () => {
     }, 2000)
   })
 })
-
-//смена темы light/dark
-const themeToggleBtn = document.getElementById('themeToggle')
-const currentTheme = localStorage.getItem('theme')
-if (currentTheme) {
-  document.documentElement.setAttribute('data-theme', currentTheme)
-} else {
-  const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches
-  if (prefersDark) {
-    document.documentElement.setAttribute('data-theme', 'dark')
-  }
-}
-themeToggleBtn.addEventListener('click', () => {
-  let theme = document.documentElement.getAttribute('data-theme')
-  if (theme === 'dark') {
-    document.documentElement.removeAttribute('data-theme')
-    localStorage.setItem('theme', 'light')
-  } else {
-    document.documentElement.setAttribute('data-theme', 'dark')
-    localStorage.setItem('theme', 'dark')
-  }
-})
